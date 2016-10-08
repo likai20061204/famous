068028c107032a615bd99e61bbc0cd814c4c9e6b
likai <287847484@qq.com>
2016-10-08 15:54:41 +0800

    提交支付宝插件

---
+++ ZHYQ/plugins/cordova-plugin-alipay/src/android/alipay/PayKeys.java
@@ -0,0 +1,23 @@
+package com.yumemor.cordova.alipay;
+
+/**
+ * 商户配置信息
+ * 
+ * @author yy
+ * 
+ */
+public final class PayKeys {
+
+	// 合作身份者id，以2088开头的16位纯数字
+	public static final String DEFAULT_PARTNER = "2088221555967259";
+
+	// 收款支付宝账号
+	public static final String DEFAULT_SELLER = "wangzhanjk@qqn.com";
+
+	// 商户私钥 自助生成
+	public static final String PRIVATE = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAKaWMxZJIgGoE1j7z8I6dtHBG11dTdTiLfjRTuhPgUv/M2ylWfCCDpGOxOy6gf5wW2hiddl/ov6bxobYKkHJ7DyYNKNWkTTi/eYLFIG4ajPOZnZ+n/bpD1kAadkN7hNoBcY3w4o7KkY+799x26V4uLmNHR6KAYwEZ/EDZNnGmYIPAgMBAAECgYEAmL5hq051MuWkj9OcBLBMoDka2TmjEs4sbtekLVgw6rjdswHN/LuTlh09LP46EU1+UrDs8/eXQTMeDigIXvhBdTcweyuKz/8SmJ1vjH6E46jbW8L5w7Q6k3aJxcRTJFo7eHB+9GfW1z9zCm3QmKLkbCNxVAZ6YqBgY2eQe2N3YsECQQDRaNH7BSBtrwWHtC4cdD5tuKKBgH8nDDDa2Y4ucT21KNt1ytyqnckixd+T2UwBq2zdrtodUf1pwsIHWI22LorhAkEAy6ZbNZAyfkCEZh3SIf1NZhoogFSrrVCn0OqnhgYR9BQ1FezzNzLNcHAuwFuokzHSeSzOpumaFhseprDoS2sa7wJAf5tgWZbGLBB7TjZ94tjzMelRHZkv4puLKdFo0uQutjYvhZgY399GseniVDYWGu5baLskLrnHsqr7kX5k3J+AAQJAXFSLC8jZq7XykR26FKD0Q3Neo3g/8a8ctrJ8y13bK+8xdkRcrM0CqZSjDaFA16OCCmBPPQuySY14IxkX6IGhzwJBAM3APDzbg1Erxl83DpuABJL890n5An8JeGsFpjt8uI/XO894pX1dOVwQJX0Q9URtvZ29UkGOTjaZWNsE+R9DmMQ=";
+
+	// 支付宝默认公钥 请勿修改
+	public static final String PUBLIC = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCmljMWSSIBqBNY+8/COnbRwRtdXU3U4i340U7oT4FL/zNspVnwgg6RjsTsuoH+cFtoYnXZf6L+m8aG2CpByew8mDSjVpE04v3mCxSBuGozzmZ2fp/26Q9ZAGnZDe4TaAXGN8OKOypGPu/fcduleLi5jR0eigGMBGfxA2TZxpmCDwIDAQAB";
+
+}
